95492881e948b693f7c878edf5699146f85a35bf
chinnawich <hotlnw159@gmail.com>
2023-08-08 18:56:59 +0700

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,50 @@
+import { useState } from "react";
 import "./App.css";
 import Board2 from "./components/Board2";
 import Board4 from "./components/Board4";
 
-
-
 function App() {
+  const [tabActive, setTabActive] = useState("tab1");
+
+
+  const handleTabActive = (e) => {
+    setTabActive(e.currentTarget.id);
+  };
   return (
-    <div className="App">
-      <div className="board ">
-        <Board2 size={9} calculateWinner={calculateWinner2} />
-      </div>
+    <div className="App h-screen">
+      <ul className=" border-b  flex-row gap-5  flex z-30 border-black transition-all mb-2 duration-700">
+        <li
+          id="tab1"
+          onClick={(e) => handleTabActive(e)}
+          className="-mb-px mr-1  cursor-pointer list-item"
+        >
+          <p
+            className={` inline-block border-l border-t border-r rounded-t py-2 px-4 font-semibold hover:text-white hover:bg-slate-700 border-black   ${
+              tabActive == "tab1" ? " text-white bg-black " : " text-black  "
+            }`}
+          >
+            3X3
+          </p>
+        </li>
+        <li
+          className="mr-1 cursor-pointer list-item"
+          id="tab2"
+          onClick={(e) => handleTabActive(e)}
+        >
+          <p
+            className={` inline-block border-l border-t border-r rounded-t py-2 px-4 font-semibold hover:text-white hover:bg-slate-700 border-black   ${
+              tabActive == "tab2" ? " text-white bg-black " : " text-black  "
+            }`}
+          >
+            4X4
+          </p>
+        </li>
+      </ul>
       <div className="board">
-        <Board4 size={16} calculateWinner={calculateWinner4} />
+        {tabActive == "tab1" ? <Board2 size={9} /> : <Board4 size={16} />}
       </div>
     </div>
   );
-}
-
-function calculateWinner2(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-
-  return null;
-}
-
-function calculateWinner4(squares) {
-  const lines = [
-    // Rows
-    [0, 1, 2, 3],
-    [4, 5, 6, 7],
-    [8, 9, 10, 11],
-    [12, 13, 14, 15],
-
-    // Columns
-    [0, 4, 8, 12],
-    [1, 5, 9, 13],
-    [2, 6, 10, 14],
-    [3, 7, 11, 15],
-
-    // Diagonals
-    [0, 5, 10, 15],
-    [3, 6, 9, 12],
-  ];
-
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c, d] = lines[i];
-    if (
-      squares[a] &&
-      squares[a] === squares[b] &&
-      squares[a] === squares[c] &&
-      squares[a] === squares[d]
-    ) {
-      return squares[a];
-    }
-  }
-
-  return null;
 }
 
 export default App;
--- src/components/Board4.jsx
+++ src/components/Board4.jsx
@@ -1,8 +1,19 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 
-function Board4({ size, calculateWinner }) {
+function Board4({ size }) {
   const [board, setBoard] = useState(Array(size).fill(null));
+
+  //Score
+  const savedScores = JSON.parse(localStorage.getItem("ticTacToeScores"));
+  const [scores, setScores] = useState(savedScores || { X: 0, O: 0 });
+
   const [xPlaying, setXPlaying] = useState(true);
+
+  const winner = calculateWinner(board);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xPlaying ? "X" : "O"}`;
 
   const handleClick = (index) => {
     if (board[index] || calculateWinner(board)) {
@@ -13,6 +24,14 @@ function Board4({ size, calculateWinner }) {
     newBoard[index] = xPlaying ? "X" : "O";
     setBoard(newBoard);
     setXPlaying(!xPlaying);
+
+    const winner = calculateWinner(newBoard);
+    if (winner) {
+      const newScores = { ...scores };
+      newScores[winner] += 1;
+      setScores(newScores);
+      localStorage.setItem('ticTacToeScores', JSON.stringify(newScores));
+    }
   };
 
   const renderSquare = (index) => (
@@ -21,25 +40,72 @@ function Board4({ size, calculateWinner }) {
     </button>
   );
 
-  const winner = calculateWinner(board);
-  const status = winner
-    ? `Winner: ${winner}`
-    : `Next player: ${xPlaying ? "X" : "O"}`;
+  const handleReset = () => {
+    setBoard(Array(size).fill(null));
+    setXPlaying(true);
+  };
+
+  function calculateWinner(squares) {
+    const lines = [
+      // Rows
+      [0, 1, 2, 3],
+      [4, 5, 6, 7],
+      [8, 9, 10, 11],
+      [12, 13, 14, 15],
+
+      // Columns
+      [0, 4, 8, 12],
+      [1, 5, 9, 13],
+      [2, 6, 10, 14],
+      [3, 7, 11, 15],
+
+      // Diagonals
+      [0, 5, 10, 15],
+      [3, 6, 9, 12],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c, d] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c] &&
+        squares[a] === squares[d]
+      ) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  }
 
   return (
     <>
-      <div className="status">{status}</div>
-      <div className="board">
-        {Array(4)
-          .fill(null)
-          .map((_, row) => (
-            <div className="row" key={row}>
-              {Array(4)
-                .fill(null)
-                .map((_, col) => renderSquare(row * 4 + col))}
-            </div>
-          ))}
+      <div>
+        <div className="status">{status}</div>
+        <div className="score fixed right-5 m-5 select-none top-0">
+          <p className=" p-3 rounded-xl bg-red-800 mb-2 text-white">Player X: {scores.X}</p>
+          <p className=" p-3 rounded-xl bg-green-800  text-white">Player O: {scores.O}</p>
+        </div>
+        <div className="board">
+          {Array(4)
+            .fill(null)
+            .map((_, row) => (
+              <div className="row" key={row}>
+                {Array(4)
+                  .fill(null)
+                  .map((_, col) => renderSquare(row * 4 + col))}
+              </div>
+            ))}
+        </div>
       </div>
+      <button
+        className="p-2 mt-5 bg-slate-700 text-white rounded-md hover:bg-slate-200 hover:text-slate-700 
+      transition-all duration-300"
+        onClick={() => handleReset()}
+      >
+        Reset
+      </button>
     </>
   );
 }
