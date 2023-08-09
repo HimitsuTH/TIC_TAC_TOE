4641c23689fbfd4a85052312126aeb7d3bf220c9
chinnawich <hotlnw159@gmail.com>
2023-08-09 18:58:13 +0700

    update custom Board

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,13 @@
-import { useState } from "react";
 import "./App.css";
-import Board3 from "./components/Board3";
-import Board4 from "./components/Board4";
+
 import CustomBoard from "./components/CustomBoard";
 
 function App() {
-  const [tabActive, setTabActive] = useState("tab1");
 
 
-  const handleTabActive = (e) => {
-    setTabActive(e.currentTarget.id);
-  };
   return (
     <div className="App h-screen">
-      <CustomBoard/>
-      <ul className=" border-b  flex-row gap-5  flex z-30 border-black transition-all mb-2 duration-700">
-        <li
-          id="tab1"
-          onClick={(e) => handleTabActive(e)}
-          className="-mb-px mr-1  cursor-pointer list-item"
-        >
-          <p
-            className={` inline-block border-l border-t border-r rounded-t py-2 px-4 font-semibold hover:text-white hover:bg-slate-700 border-black   ${
-              tabActive == "tab1" ? " text-white bg-black " : " text-black  "
-            }`}
-          >
-            3X3
-          </p>
-        </li>
-        <li
-          className="mr-1 cursor-pointer list-item"
-          id="tab2"
-          onClick={(e) => handleTabActive(e)}
-        >
-          <p
-            className={` inline-block border-l border-t border-r rounded-t py-2 px-4 font-semibold hover:text-white hover:bg-slate-700 border-black   ${
-              tabActive == "tab2" ? " text-white bg-black " : " text-black  "
-            }`}
-          >
-            4X4
-          </p>
-        </li>
-      </ul>
-      <div className="board">
-        {tabActive == "tab1" ? <Board3 size={9} /> : <Board4 size={16} />}
-      </div>
+      <CustomBoard />
     </div>
   );
 }
--- src/components/CustomBoard.jsx
+++ src/components/CustomBoard.jsx
@@ -1,19 +1,46 @@
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
+import Modal from "./Modal";
 
 // console.log(calculateWinner)
 
 function CustomBoard() {
-  const [boardSize, setBoardSize] = useState(4); // Default board size
-  const [history, setHistory] = useState([
-    Array(boardSize * boardSize).fill(null),
-  ]);
+  const [history, setHistory] = useState([]);
 
-  const [board, setBoard] = useState(Array(boardSize * boardSize).fill(null | 0));
-  const [scores, setScores] = useState({ X: 0, O: 0 });
-  const [stepNumber, setStepNumber] = useState(0);
+  const [inputSize, setInputSize] = useState(3);
+  const [size, setSize] = useState(3);
+
+  const handleInputChange = (event) => {
+    const newValue = event.target.value;
+
+    // Check if the new value is a valid number
+    if (!isNaN(newValue)) {
+      setInputSize(parseInt(newValue));
+    }
+  };
+
+  const [board, setBoard] = useState(Array(size * size).fill(null | 0));
+  //Score
+  const savedScores = JSON.parse(localStorage.getItem("ticTacToeScores"));
+  const [scores, setScores] = useState(savedScores || { X: 0, O: 0 });
+
   const [showModal, setShowModal] = useState(false);
   const [xIsNext, setXIsNext] = useState(true);
+
+  const handleCustomBoard = () => {
+    if (inputSize < 3) {
+      alert("Board size must be at least 3.");
+
+      return;
+    } else if (inputSize > 6) {
+      alert("board size up to 6");
+
+      return;
+    } else {
+      setSize(inputSize);
+      handleReset();
+    }
+  };
 
   function calculateWinner(squares, size) {
     const lines = [];
@@ -22,35 +49,36 @@ function CustomBoard() {
     for (let row = 0; row < size; row++) {
       lines.push(Array.from({ length: size }, (_, col) => row * size + col));
     }
-  
+
     // Generate winning combinations for columns
     for (let col = 0; col < size; col++) {
       lines.push(Array.from({ length: size }, (_, row) => row * size + col));
     }
-  
+
     // Generate winning combinations for diagonals
     lines.push(Array.from({ length: size }, (_, i) => i * (size + 1)));
     lines.push(Array.from({ length: size }, (_, i) => (i + 1) * (size - 1)));
-    console.log(lines)
+
     // Check for a winner in each line
     for (const line of lines) {
-        const symbols = line.map(index => squares[index]);
-        const allEqual = symbols.every(symbol => symbol && symbol === symbols[0]);
-        if (allEqual) {
-          return symbols[0];
-        }
+      const symbols = line.map((index) => squares[index]);
+
+      // console.log(symbols)
+
+      const allEqual = symbols.every(
+        (symbol) => symbol && symbol === symbols[0]
+      );
+      if (allEqual) {
+        return symbols[0];
       }
-    
-      return null;
+    }
+
+    return null;
   }
 
   const handleClick = (index) => {
-    if (board[index] || calculateWinner(board, boardSize)) {
+    if (board[index] || calculateWinner(board, size)) {
       return;
-    }
-    console.log(index);
-    {
-    //   console.log(board[0]);
     }
 
     const newBoard = board.slice();
@@ -60,7 +88,7 @@ function CustomBoard() {
 
     setXIsNext(!xIsNext);
 
-    const winner = calculateWinner(newBoard, boardSize);
+    const winner = calculateWinner(newBoard, size);
     if (winner) {
       setShowModal(true);
       const newScores = { ...scores };
@@ -70,31 +98,12 @@ function CustomBoard() {
     }
   };
 
-  const current = board[stepNumber];
-  const winner = calculateWinner(current, boardSize);
+  // get winner
+  const winner = calculateWinner(board, size);
+  // status game
   const status = winner
     ? `Winner: ${winner}`
-    : `Next player: ${xIsNext ? "X" : "O"}`;
-
-  const handleCustomBoard = () => {
-    if (boardSize >= 3) {
-      setHistory([Array(boardSize * boardSize).fill(null)]);
-      setStepNumber(0);
-      setXIsNext(true);
-      setScores({ X: 0, O: 0 });
-      localStorage.setItem(
-        "ticTacToeHistory",
-        JSON.stringify([Array(boardSize * boardSize).fill(null)])
-      );
-      localStorage.setItem("ticTacToeScores", JSON.stringify({ X: 0, O: 0 }));
-    }else if(boardSize == undefined || boardSize == null){
-        setBoard((Array(3 * 3).fill(null | 0)))
-    } 
-    
-    else {
-      alert("Board size must be at least 3.");
-    }
-  };
+    : `Next player : "${xIsNext ? "X" : "O"}"`;
 
   //Square
   const renderSquare = (index) => (
@@ -109,33 +118,69 @@ function CustomBoard() {
     </button>
   );
 
+  //@Reset Board
+  const handleReset = () => {
+    setBoard(Array(size).fill(null));
+    setXIsNext(true);
+    setScores({ X: 0, O: 0 });
+    // setHistoryPlay([]);
+    localStorage.removeItem("ticTacToeScores");
+  };
+
+  const handleNewGame = () => {
+    setBoard(Array(size).fill(null));
+    setXIsNext(true);
+    setHistory([]);
+  };
+
+  //Close Modal Alert Winner Or Draw
+  const closeModal = () => {
+    handleNewGame();
+    setShowModal(false);
+  };
+
   return (
-    <div className="CustomBoard">
-      <div>
+    <div className="flex flex-col justify-center items-center">
+      {showModal && (
+        <Modal closeModal={closeModal} winner={winner ? winner : "Draw"} />
+      )}
+
+      <div className="score fixed right-5 m-5 select-none top-0">
+        <p className=" p-3 rounded-xl bg-red-800 mb-2 text-white">
+          Player X: {scores.X}
+        </p>
+        <p className=" p-3 rounded-xl bg-green-800  text-white">
+          Player O: {scores.O}
+        </p>
+      </div>
+      <div className="status text-lg font-bold select-none text-white bg-slate-800 p-5 rounded-lg">{status}</div>
+
+      <div className="board">
+        {Array(size)
+          .fill(null)
+          .map((_, row) => (
+            <div className="row" key={row}>
+              {Array(size)
+                .fill(null)
+                .map((_, col) => renderSquare(row * size + col))}
+            </div>
+          ))}
+      </div>
+      <div className=" flex gap-x-5  p-1 items-center text-xs md:text-base">
         <label htmlFor="boardSize">Enter board size: </label>
         <input
           type="number"
           id="boardSize"
-          value={boardSize}
-          onChange={(e) => setBoardSize(parseInt(e.target.value))}
+          className=" text-center rounded-lg "
+          value={inputSize}
+          onChange={handleInputChange}
         />
-        <button onClick={handleCustomBoard}>Set Board Size</button>
-      </div>
-      <div className="status">{status}</div>
-      <div className="score">
-        <p>Player X: {scores.X}</p>
-        <p>Player O: {scores.O}</p>
-      </div>
-      <div className="board">
-        {Array(boardSize)
-          .fill(null)
-          .map((_, row) => (
-            <div className="row" key={row}>
-              {Array(boardSize)
-                .fill(null)
-                .map((_, col) => renderSquare(row * boardSize + col))}
-            </div>
-          ))}
+        <button
+          onClick={handleCustomBoard}
+          className=" bg-blue-600 p-2 rounded-xl text-white hover:bg-blue-700 hover:shadow-blue-400 shadow-md "
+        >
+          Set Board
+        </button>
       </div>
     </div>
   );
